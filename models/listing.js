22c43160b9fae8b05e0bcd3992af073f269ea48d
FakeShemp <sakcheen@gmail.com>
2019-06-12 10:06:48 +0200

    Update swagger

--- models/listing.js
+++ models/listing.js
@@ -1,7 +1,43 @@
 mongoose = require('mongoose');
 
-const listingSchema = new mongoose.Schema({
-});
+const listingSchema = new mongoose.Schema([
+    {
+        coordinates: {
+            lng: {
+                type: Number
+            },
+            lat: {
+                type: Number
+            }
+        },
+        street: {
+            type: String,
+            required: true
+        },
+        location: {
+            municipality: {
+                type: String
+            },
+            city: {
+                type: String
+            }
+        },
+        type: {
+            type: String,
+            required: true
+        },
+        price: {
+            type: Number
+        },
+        monthly_fee: {
+            type: Number
+        },
+        active: {
+            type: Boolean,
+            required: true
+        },
+    }
+]);
 
 const Listing = mongoose.model('Listing', listingSchema);
 
